c7e50486d80a255bbff6beea5c1d7edffd82ba9b
alex2rive3 <alex2rive3@gmail.com>
2023-01-11 22:55:31 -0300

    Bloques de html con estilos css con react

---
+++ src/Components/Advertisement.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Advertisement extends Component {
+    render() {
+        return <div className="advertisements"></div>;
+    }
+}
+
+export default Advertisement;
